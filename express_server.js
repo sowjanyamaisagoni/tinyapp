b072cb8d57082afd4903db658a39de362d6706f2
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-30 19:55:04 -0400

    adding urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,13 +20,23 @@ app.get("/urls.json", (req, res) => {
  });
 
  app.get("/set", (req, res) => {
-   const a = 1;
+   const a = 1;   
    res.send(`a = ${a}`);
   });
   
   app.get("/fetch", (req, res) => {
    res.send(`a = ${a}`);
   });
+
+  app.get("/urls", (req, res) => {
+   const templateVars = { urls: urlDatabase };
+   res.render("urls_index", templateVars);
+ });
+
+ app.get("/hello", (req, res) => {
+   const templateVars = { greeting: 'Hello World!' };
+   res.render("hello_world", templateVars);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
