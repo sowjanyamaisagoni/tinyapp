9acd2d48c5b38a63751506cfd2354f5b9a26d788
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-08-31 14:31:34 -0400

    adding logURl

--- express_server.js
+++ express_server.js
@@ -66,13 +66,17 @@ app.get("/urls.json", (req, res) => {
  app.get("/urls/new", (req, res) => {
    res.render("urls_new");
  });
- 
+
  app.get("/urls/:shortURL", (req, res) => {
    const templateVars = { shortURL: req.params.shortURL, longURL:' ' };
    res.render("urls_show", templateVars);
  });
 
- 
+ app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL)
+})
+
 
  app.post("/urls", (req, res) => {
    console.log(req.body);  // Log the POST request body to the console
